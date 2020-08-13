5af2ae155e9a01f86b8f05ebd0cfb573e3a3cbfa
Nashmeyah <nashalrekabi@gmail.com>
2020-08-13 11:14:19 -0600

    Minor adjustments

--- story-line-frontend/src/components/CharacterCard.js
+++ story-line-frontend/src/components/CharacterCard.js
@@ -5,9 +5,16 @@ const CharacterCard = ({ story }) => {
   // console.log(story);
   const characters = story.map((char, index) => (
     <div key={index}>
+      <h1>Name:</h1>
       <h3>{char.name}</h3>
+      <h1>Age:</h1>
+
       <p>{char.age}</p>
+      <h1>Sex:</h1>
+
       <p>{char.sex}</p>
+      <h1>History:</h1>
+
       <p>{char.history}</p>
     </div>
   ));

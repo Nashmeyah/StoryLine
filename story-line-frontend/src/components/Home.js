22dd6b6d03960e7394452747954801bff2cbe117
Nashmeyah <nashalrekabi@gmail.com>
2020-08-18 00:12:06 -0600

    Changed Hom.js from class component to functional

--- story-line-frontend/src/components/Home.js
+++ story-line-frontend/src/components/Home.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome to my app! Nancy says have fun :D!!</h1>
-      </div>
-    );
-  }
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to my app! Nancy says have fun :D!!</h1>
+    </div>
+  );
 }
 
 export default Home;

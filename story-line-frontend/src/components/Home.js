96355643b3631f6bc5341fe387febe590651dce1
Nashmeyah <nashalrekabi@gmail.com>
2020-11-20 20:38:27 -0700

    changed css properties and changed background img

--- story-line-frontend/src/components/Home.js
+++ story-line-frontend/src/components/Home.js
@@ -2,10 +2,14 @@ import React from "react";
 import Header from "./Header";
 
 const Home = () => {
+  const style = {
+    color: "white",
+    fontSize: "30px",
+  };
   return (
     <div>
       <Header />
-      <h2>
+      <h2 style={style}>
         Welcome to the StoryLine creator, this app allows you to create stories
         and share them when youre ready!
       </h2>

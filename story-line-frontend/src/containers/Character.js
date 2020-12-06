7b9836241ba0fd028b9b32964d800434425795e5
Nashmeyah <nashalrekabi@gmail.com>
2020-12-06 02:19:45 -0700

    Adding characters list style

--- story-line-frontend/src/containers/Character.js
+++ story-line-frontend/src/containers/Character.js
@@ -13,6 +13,9 @@ class Character extends Component {
   };
 
   render() {
+    const characters_list_style = {
+      display: "flex",
+    };
     // console.log(this.props.match);
 
     const stories = this.props.stories.find(
@@ -25,7 +28,9 @@ class Character extends Component {
     return (
       <div>
         <button onClick={this.showForm}>Add New Character</button>
-        {stories && <CharacterCard story={stories.characters} />}
+        <div style={characters_list_style}>
+          {stories && <CharacterCard story={stories.characters} />}
+        </div>
       </div>
     );
   }

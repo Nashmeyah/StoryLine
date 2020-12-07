137ea9f28acacc69e2727daeda5c64eff96fb087
Nashmeyah <nashalrekabi@gmail.com>
2020-12-06 20:41:34 -0700

    Centered content, added padding

--- story-line-frontend/src/components/CharacterCard.js
+++ story-line-frontend/src/components/CharacterCard.js
@@ -4,12 +4,17 @@ import React from "react";
 const CharacterCard = ({ story }) => {
   const style = {
     display: "flex",
-    flexDirection: "row",
-    color: "white",
+    padding: "15px",
+    border: "10px",
+    justifyContent: "center",
+  };
+
+  const charstyle = {
+    padding: "15px",
   };
   // console.log(story);
   const characters = story.map((char, index) => (
-    <div style={style} key={index}>
+    <div style={charstyle} key={index}>
       <p>Here is your characters</p>
       <h1>Name:</h1>
       <h3>{char.name}</h3>
@@ -25,7 +30,7 @@ const CharacterCard = ({ story }) => {
     </div>
   ));
 
-  return <div>{characters}</div>;
+  return <div style={style}>{characters}</div>;
 };
 
 export default CharacterCard;

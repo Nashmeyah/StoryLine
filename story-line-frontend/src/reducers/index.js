5fc514e8ad5254e33c1431662c4b88b20d0ccd2e
Nashmeyah <nashalrekabi@gmail.com>
2020-12-07 23:48:00 -0700

    added a charReducer for input from user for char

--- story-line-frontend/src/reducers/index.js
+++ story-line-frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import storyReducer from "./storyReducer";
+import characterReducer from "./characterReducer";
 
 const rootReducer = combineReducers({
   storyReducer,
+  characterReducer,
 });
 
 export default rootReducer;

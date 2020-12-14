791b60afca742643c03386de21bf55f6f695613b
Nashmeyah <nashalrekabi@gmail.com>
2020-12-13 18:00:13 -0700

    created component and created charForm

---
+++ story-line-frontend/src/components/CharacterForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { addCharacter } from "../actions/stories";
+import { Input } from "antd";
+
+const CharacterForm = (props) => {
+  const [charName, setcharName] = useState("");
+  const [charAge, setcharAge] = useState("");
+  const [sex, setsex] = useState("");
+  const [history, sethistory] = useState("");
+
+  const handlecharName = (event) => {
+    setcharName(event.target.value);
+  };
+  const handlesetcharAge = (event) => {
+    setcharAge(event.target.value);
+  };
+  const handlesetsex = (event) => {
+    setsex(event.target.value);
+  };
+  const handlesethistory = (event) => {
+    sethistory(event.target.value);
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    const character = {
+      charName,
+      charAge,
+      sex,
+      history,
+      storyId: props.match.params.id,
+    };
+    //having trouble connecting to the action.
+    props.addCharacter(character);
+  };
+
+  return (
+    <div>
+      <p>Add Character</p>
+      <br></br>
+      <form id="add-char" onSubmit={handleOnSubmit}>
+        <label>Character Name: </label>
+        <Input onChange={handlecharName} type="text" name="charName"></Input>
+
+        <label>Age: </label>
+        <Input onChange={handlesetcharAge} type="text" name="charAge"></Input>
+
+        <label>Sex: </label>
+        <Input onChange={handlesetsex} type="text" name="sex"></Input>
+
+        <label>History: </label>
+        <Input onChange={handlesethistory} type="text" name="history"></Input>
+        <input type="Submit" />
+      </form>
+    </div>
+  );
+};
+export default CharacterForm;
--- story-line-frontend/src/containers/Character.js
+++ story-line-frontend/src/containers/Character.js
@@ -2,91 +2,23 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getStories } from "../actions/stories";
 import CharacterCard from "../components/CharacterCard";
-import { Input } from "antd";
+import CharacterForm from "../components/CharacterForm";
+
+// import StoryForm from "../components/StoryForm";
 
 class Character extends Component {
-  constructor(props) {
-    super(props);
-    // Here we initialize our components state
-    this.state = {
-      showForm: false,
-      charName: "",
-      charAge: "",
-      sex: "",
-      history: "",
-    };
-    this.onClick = this.onClick.bind(this);
-  }
-
   componentDidMount() {
     this.props.getStories();
-    console.log(this.props);
   }
-  onClick() {
-    // On click we change our state – this will trigger our `render` method
-    this.setState({ showForm: true });
-  }
-
-  handleInput = (event) => {
-    event.persist();
-    // console.log(event);
-    const { value, name } = event.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleOnSubmit(event) {
-    event.preventDefault();
-    const character = {
-      charName: this.state.charName,
-      charAge: this.state.charAge,
-      sex: this.state.sex,
-      history: this.state.history,
-      storyId: this.props.match.params.id,
-    };
-    //having trouble connecting to the action.
-    this.props.addCharacter(character);
-  }
-
-  showForm = () => {
-    return (
-      <div>
-        <br></br>
-        <form id="add-char">
-          <label>Character Name: </label>
-          <Input
-            onChange={this.handleInput}
-            type="text"
-            name="charName"
-          ></Input>
-
-          <label>Age: </label>
-          <Input onChange={this.handleInput} type="text" name="charAge"></Input>
-
-          <label>Sex: </label>
-          <Input onChange={this.handleInput} type="text" name="sex"></Input>
-
-          <label>History: </label>
-          <Input onChange={this.handleInput} type="text" name="history"></Input>
-          <button>Create</button>
-        </form>
-      </div>
-    );
-  };
 
   render() {
     const stories = this.props.stories.find(
       (story) => story.id == this.props.match.params.id
     );
 
-    // debugger;
-    // console.log(stories);
-
     return (
       <div>
-        <button onClick={this.onClick}>Add New Character</button>
-        {this.state.showForm && this.showForm()}
+        <CharacterForm />
         <div>{stories && <CharacterCard story={stories.characters} />}</div>
       </div>
     );
